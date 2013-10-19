3911f38765f8797e9ebab36bbfc17a10108402e1
Sagie Levy <sagielevy21@gmail.com>
2013-10-19 15:07:44 +0300

    Change consts

--- src/com/tas/icecaveLibrary/general/Consts.java
+++ src/com/tas/icecaveLibrary/general/Consts.java
@@ -11,24 +11,23 @@ public class Consts
 	public final static String THEME_SELECT_TAG = "themeSelect";
 	public final static String SELECT_BOARD_SIZE_SIZE = "selectBoardSize";
 	public final static String MUSIC_MUTE_FLAG = "musicMuteFlag";
-	public final static String RESET_SHARED_DATA_TAG = "ResetSharedDataTag"; 
-	
+	public final static String RESET_SHARED_DATA_TAG = "ResetSharedDataTag";
+
 	public final static Point DEFAULT_START_POS = new Point(1, 1);
-	
+
 	// Updates
 	public static final int PLAYER_FINISH_MOVE_UPDATE = 0;
 	public static final int LOADING_LEVEL_FINISHED_UPDATE = 1;
-	
+
 	/***
 	 * Level default values
 	 */
-	// Easy
 	public final static int DEFAULT_WALL_WIDTH = 1;
 	public final static int DEFAULT_BOULDER_RELATION = 7;
 	public final static int DEFAULT_BOARD_SIZE = 14;
-	
+
 	/***
-	 *  Standard tile values and positioning
+	 * Standard tile values and positioning
 	 */
 	// Player tiles
 	public final static int PLAYER_ERROR_ROW = -1;
@@ -40,43 +39,44 @@ public class Consts
 	public final static int PLAYER_STANDING = 1;
 	public final static int PLAYER_MOVING_2 = 2;
 	public final static int PLAYER_MOVEMENTS_SUM = 3; // TODO Might want to change these into an enum
-	
-	// Board tiles
-	public final static Point EMPTY_TILE_IN_SPRITE = new Point(0, 0);
-	public final static Point FLAG_TILE_IN_SPRITE = new Point(1, 0);
-	public final static Point BOULDER_TILE_IN_SPRITE = new Point(2, 0);
-	public final static Point WALL_TILE_IN_SPRITE = new Point(3, 0);
-	
+
 	// Columns and rows
 	public final static int DEFAULT_TILES_BMP_COLUMNS = 8;
 	public final static int DEFAULT_TILES_BMP_ROWS = 2;
 	public final static int DEFAULT_PLAYER_BMP_ROWS = 4;
 	public final static int DEFAULT_PLAYER_BMP_COLUMNS = 3;
-	
+
 	/***
 	 * Absolute Paths
 	 */
-	
+
 	// Styles
+	public final static String STYLE_PIXELART = "fonts/pixelart.ttf";
 	public final static String STYLE_SNOW_TOP = "fonts/SnowtopCaps.otf";
 	public final static String STYLE_ROBOTO_THIN = "fonts/Roboto-Thin.ttf";
 	public final static String STYLE_ROBOTO_BLACK = "fonts/Roboto-Black.ttf";
 	public final static String STYLE_ROBOTO_CONDENSED_LIGHT = "fonts/RobotoCondensed-Light.ttf";
-	
+
 	/**
 	 * Special keys for flags
 	 */
-	
+
 	// Difficulty lock
 	public final static String LOCK_HARD_DIFFICULTY = "LockHardDifficulty";
-	
+
 	/**
 	 * Versions
 	 */
-	
+
 	// Map Generator Version
 	public final static String MAP_GEN_VERSION = "1.0";
-	
+
 	// Number of resets throughout the different versions so far
 	public final static int RESET_NUMBER = 1;
+
+	/**
+	 * Package file names
+	 */
+	public final static String[] PACKAGE_NAMES = new String[]
+		{ "Cave Entrance", "Hidden Dungeon", "Frozen Caverns", "Ancient Tombs" };
 }
